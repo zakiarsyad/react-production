00871fc409c9c3b33167b1999a8ce5bb0f653a54
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 19:58:15 +0700

    initial app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Github Netlify Deployment
         </p>
         <a
           className="App-link"
