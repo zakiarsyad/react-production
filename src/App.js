a1ccc8febcb2b25ba59a4b472100e1fde79763b7
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 20:43:45 +0700

    change content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Local Deployment
+          Firebase automatic deployment thru Github
         </p>
         <a
           className="App-link"
