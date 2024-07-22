cf96a062119dc23578127227e6badff668776682
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 20:03:53 +0700

    deploy using cli

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Netlify Deployment
+          Netlify CLI Deployment
         </p>
         <a
           className="App-link"
