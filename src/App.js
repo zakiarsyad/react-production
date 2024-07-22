f11f91727c95dfef7a103a7a1c3cab334004ef83
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 20:42:55 +0700

    firebase config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Netlify CLI Deployment
+          Local Deployment
         </p>
         <a
           className="App-link"
