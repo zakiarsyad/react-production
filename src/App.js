45b1480a2cd87ab8fc00f1bb75d67c2c38831213
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 20:52:30 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          PR auto merge
+          Firebase automatic deployment after PR is merged
         </p>
         <a
           className="App-link"
